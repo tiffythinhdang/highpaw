54f69c6c38bde1c124a03af50d5f308b4df18c3a
Thinh Dang <tiffythinhdang@gmail.com>
2019-09-22 20:32:15 -0700

    Set up db sucessfully

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const db = require('./config/keys').mongoURI;
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
