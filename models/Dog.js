74c63879b1003385437b17c6cdcae7655c6ef2da
Thinh Dang <tiffythinhdang@gmail.com>
2019-09-22 21:09:12 -0700

    Add dogs model

--- models/Dog.js
+++ models/Dog.js
@@ -1,6 +1,34 @@
-DOG TABLE
-name
-age
-breed
-gender['male', 'female', 'other', 'unknown']
-owner_id
+// DOG TABLE
+// name
+// age
+// breed
+// gender['male', 'female', 'other', 'unknown']
+// owner_id
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const DogSchema = new Schema({
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true
+  },
+  breed: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = Dog = mongoose.model('dogs', DogSchema);
