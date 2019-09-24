0ae86b1c8ec204390171dbc6fd25518a44f1eb93
Thinh Dang <tiffythinhdang@gmail.com>
2019-09-24 12:42:56 -0700

    Add dogs_api_util

---
+++ frontend/src/util/dogs_api_util.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export const fetchDogsFromWalk = (walkId) => {
+  return axios.get(`api/dogs/walks/${walkId}`);
+};
+
+export const fetchDogsFromUser = (userId) => {
+  return axios.get(`api/dogs/user/${userId}`);
+};
+
+export const fetchADog = (id) => {
+  return axios.get(`/api/dogs/${id}`);
+};
+
+export const createADog = (data) => {
+  return axios.get('/api/dogs/', data);
+};
+
+export const deleteADog = (id) => {
+  return axios.delete(`/api/dogs/${id}`);
+};
