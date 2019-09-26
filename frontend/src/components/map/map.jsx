a071b7788c545c4faf66dc6c13b5bb26821d8ccb
Jason Liao <jasonliao@live.com>
2019-09-25 23:12:25 -0700

    Adds Websockets to send location

--- frontend/src/components/map/map.jsx
+++ frontend/src/components/map/map.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 const google = window.google
+const io = require('socket.io-client');
+const port = process.env.PORT || 5000;
+
+let walks = io.connect(process.env.PORT ? `http://highpaw.herokuapp.com:${process.env.PORT + 1}` : `http://localhost:${port + 1}/walks`)
+
+walks.on('welcome', (msg) => {
+  console.log('Received: ', msg)
+})
+
+walks.emit('joinRoom', 'testing')
+
+walks.on('success', (res) => console.log(res))
+
+walks.on('sendMessage', message => {
+  console.log(message)
+});
+
+
+// walks.emit('sendText', msg )
+
+window.walks = walks
 
 export default class Map extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      markers: []
+      marker: false
     }
   }
 
@@ -24,15 +45,29 @@ export default class Map extends React.Component {
     const locationTag = document.getElementById('demo');
 
     if (navigator.geolocation) {
-      navigator.geolocation.watchPosition((position) => {
-        let latLng = { lat: position.coords.latitude, lng: position.coords.longitude }
-        const marker = new google.maps.Marker({
-          position: latLng,
-          map: this.map
-        })
+      
+      setInterval(() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+          let latLng = { lat: position.coords.latitude, lng: position.coords.longitude }
 
-        this.map.setCenter(latLng)
-      })
+          if (this.state.marker) {
+            // console.log(this.state.marker)
+            this.state.marker.setPosition(latLng)
+            
+          } else {
+            // console.log("new mark")
+            let marker = new google.maps.Marker({
+              position: latLng,
+              map: this.map
+            })
+            this.setState({
+              marker
+            })
+            
+            this.map.setCenter(latLng)
+          }    
+        })
+      }, 1000)
     } else {
       locationTag.innerHTML = "Geolocation isn't supported by your browser."
     }
@@ -43,20 +78,40 @@ export default class Map extends React.Component {
   componentDidUpdate() {
     const locationTag = document.getElementById('demo');
 
-    if (navigator.geolocation) {
-      navigator.geolocation.watchPosition((position) => {
-        let latLng = { lat: position.coords.latitude, lng: position.coords.longitude }
-        
-        const marker = new google.maps.Marker({
-          position: latLng,
-          map: this.map
-        })
+    // if (navigator.geolocation) {
+    //   this.maker = false
+    //   setInterval(() => {
+    //     navigator.geolocation.getCurrentPosition((position) => {
+    //       let latLng = { lat: position.coords.latitude, lng: position.coords.longitude }
 
-        this.map.setCenter(latLng)
-      })
-    } else {
-      locationTag.innerHTML = "Geolocation isn't supported by your browser."
-    }
+        
+    //       if (marker) {
+    //         marker.setCenter(latLng)
+    //       } else {
+    //         marker = new google.maps.Marker({
+    //           position: latLng,
+    //           map: this.map
+    //         })
+    //       }
+
+    //       // this.map.setCenter(latLng)
+    //     })
+    //   }, 1000)
+
+
+      // navigator.geolocation.watchPosition((position) => {
+      //   let latLng = { lat: position.coords.latitude, lng: position.coords.longitude }
+        
+      //   const marker = new google.maps.Marker({
+      //     position: latLng,
+      //     map: this.map
+      //   })
+
+      //   this.map.setCenter(latLng)
+      // })
+    // } else {
+    //   locationTag.innerHTML = "Geolocation isn't supported by your browser."
+    // }
   }
 
 
