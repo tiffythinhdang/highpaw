2bd2aeb68d254bd09a8a4fb4bb69073d7787936e
Jason Liao <jasonliao@live.com>
2019-09-28 20:38:09 -0700

    Attempt to fix heroku error

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -17,3 +17,25 @@ const configureStore = (preloadedState = {}) => {
 };
 
 export default configureStore;
+
+
+
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers/root_reducer';
+
+
+const configureStore = (preloadedState = {}) => {
+  let middleware = [thunk];
+  if (process.env.NODE_ENV !== 'production') {
+    middleware = [...middleware, logger];
+  }
+  return createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(...middleware)
+  );
+};
+
+export default configureStore;
